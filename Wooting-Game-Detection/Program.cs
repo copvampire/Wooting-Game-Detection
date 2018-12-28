6de4a2052866ecca8fcd59ad475ae030ed41110b
BigBrainAFK <tony.langhammer@yahoo.de>
2018-12-28 21:00:19 +0100

    Removed unnecessary using

--- Wooting-Game-Detection/Program.cs
+++ Wooting-Game-Detection/Program.cs
@@ -5,8 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Wooting_Game_Detection
 {
